9509ba696bc6639b11ba1d7832a70e2394eedcbf
솜놜 <90139018+somnwal@users.noreply.github.com>
2025-06-07 01:15:51 +0900

    fix: bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import {routes} from "./routes/routes.tsx";
 import {Suspense} from "react";
 import "./index.css";
-import WithParkUIProvider from "@withpark/ui/components/Provider/WithParkUIProvider";
 
 const router = createBrowserRouter(routes)
 

78f637aa0e87673bfc8ca9167b1e29d5b5b3cb8a
솜놜 <90139018+somnal-dev@users.noreply.github.com>
2025-09-29 23:03:28 +0900

    Update config.ts

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,3 +1,1 @@
-export const API_URL = import.meta.env.PROD
-  ? "/api" // 프로덕션에서는 Vercel 프록시 사용
-  : `${import.meta.env.VITE_SERVER_URL}/api`; // 개발환경에서는 직접 연결
+export const API_URL = `${import.meta.env.VITE_SERVER_URL}/api`;
